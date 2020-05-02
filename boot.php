55ac33b0420dfdfc0ba0fa12287d2dd3f6601fc3
djbusby <code@edoceo.com>
2020-05-02 23:13:45 +0000

    update css

--- boot.php
+++ boot.php
@@ -20,12 +20,3 @@
 define('APP_ROOT', dirname(__FILE__));
 
 require_once(APP_ROOT . '/vendor/autoload.php');
-
-function _text_stub($x)
-{
-	$x = strtolower($x);
-	$x = preg_replace('/[^\w\-]+/', '-', $x);
-	$x = preg_replace('/\-+/', '-', $x);
-	$x = trim($x, '-');
-	return $x;
-}
