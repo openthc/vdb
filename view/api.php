14ccbd5ec7d0d26657b7040a91bb8a989dc3465c
openthc <code@openthc.com>
2023-08-09 20:46:50 +0000

    update api docs

--- view/api.php
+++ view/api.php
@@ -8,7 +8,7 @@
 </div>
 
 
-<div class="container mx-auto min-h-[20rem]">
+<div class="container mx-auto min-h-screen">
 
 <section class="pb-2">
 
@@ -17,6 +17,20 @@
 
 	<pre><code>GET /api/autocomplete?q=TERM</code></pre>
 	<pre><code>GET /api/autocomplete?term=TERM</code></pre>
+	<pre><code>[
+	{
+		"code": "",
+		"stub": "",
+		"label": "Strain A",
+		"value": "Strain A",
+	},
+	{
+		"code": "",
+		"stub": "",
+		"label": "Strain B",
+		"value": "Strain B",
+	},
+]</code></pre>
 
 </section>
 
@@ -24,7 +38,50 @@
 	<h2 class="h1">/api/search</h2>
 	<p>This end-point is for getting a response in the OpenTHC JSON schema.</p>
 
-	<pre><code>GET /api/search?q=TERM</code></pre>
+	<pre><code>GET /api/search?q=TERM
+[
+	{
+		"id": "",
+		"code": "",
+		"stub": "",
+		"name": "Strain A",
+	},
+	{
+		"id": "",
+		"code": "",
+		"stub": "",
+		"name": "Strain B",
+	}
+]</code></pre>
+
 </section>
+
+
+<h2>Create a Strain</h2>
+
+<pre>
+POST /api/create
+
+&gt;&gt;&gt;&gt;
+{
+        "name": "Strain C",
+        "type": "",
+}
+####
+</pre>
+
+<h2>Update a Strain</h2>
+
+<pre>
+POST /api/update/S123.D
+
+&gt;&gt;&gt;&gt;
+{
+        "name": "Strain D",
+        "type": "",
+}
+####
+</pre>
+
 
 </div>
